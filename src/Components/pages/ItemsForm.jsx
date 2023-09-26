b5c2b454f76aae3178edd51cc2db4ba051e3acd6
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-27 00:56:31 +0300

    Added Handler for Creating Items

--- src/Components/pages/ItemsForm.jsx
+++ src/Components/pages/ItemsForm.jsx
@@ -1,61 +1,69 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import testImg from '../../assets/images/trouser.jpg'
 import axios from "axios";
+import { useParams} from "react-router;"
+
+
 
 const ItemsForm = () => {
 
-
+    
     const [itemName,setItemName] = useState('');
     const [quantity,setQuantity] = useState('');
     const [notes, setNotes] = useState('');
 
 
-    const dataHandler = async(ev) => {
-        ev.preventDefault();
+    //list item creation
 
-        const itemData = {itemName, quantity, notes}
+    async function createShoppingItem(event){
+        event.preventDefault();
 
-        // if (id) {
-        //     //its an update to existing list
-        //     await axios.put('/items',{id,...itemData})
-        // } else {
-        //     await axios.post('/items',itemData);
-
-        // }
+        try {
+            await axios.post('/item',{
+                itemName,
+                quantity,
+                notes
+            });
+        } catch (e) {
+            alert ('Item creation failed. ')
+        }
     }
+
+    
 
     return (
 
-    <div className="mx-auto">
-        <div className="py-3 px-4">
-            <label>Item Name</label>
-            <input 
-                type="text"
-                className="w-full border my-2 py-2 px-2 rounded-2xl"
-                value={itemName}
-                onchange={ev => setItemName(ev.target.value)}
-            />
-        </div>
-        <div className="py-3 px-4">
-            <label>Quantity</label>
-            <input 
-                type="text"
-                className="w-full border my-2 py-2 px-2 rounded-2xl"
-                value={quantity}
-                onchange={ev => setQuantity(ev.target.value)}
-            />
-        </div><div className="py-3 px-4">
-            <label>Notes</label>
-            <textarea 
-                type="text"
-                className="block h-20 border"
-                value={notes}
-                onchange={ev => setNotes(ev.target.value)}
-            />
-        </div>
-        <button onClick={dataHandler} className="py-2 px-2 bg-red-500 text-white rounded-2xl">
-            Add to list
-        </button>
-    </div>
+        <form onSubmit={createShoppingItem}>
+            <div className="py-3 px-4">
+                <label>Item Name</label>
+                <input 
+                    type="text"
+                    className="w-full border my-2 py-2 px-2 rounded-2xl"
+                    value={itemName}
+                    onchange={ev => setItemName(ev.target.value)}
+                />
+            </div>
+            <div className="py-3 px-4">
+                <label>Quantity</label>
+                <input 
+                    type="text"
+                    className="w-full border my-2 py-2 px-2 rounded-2xl"
+                    value={quantity}
+                    onchange={ev => setQuantity(ev.target.value)}
+                />
+            </div><div className="py-3 px-4">
+                <label>Notes</label>
+                <textarea 
+                    type="text"
+                    className="block h-20 border"
+                    value={notes}
+                    onchange={ev => setNotes(ev.target.value)}
+                />
+            </div>
+            <button  className="py-2 px-2 bg-red-500 text-white rounded-2xl">
+                Add to list
+            </button>
+        </form>
     )
 
 }
