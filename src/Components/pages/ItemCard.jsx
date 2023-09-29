55067193556d37debea3582d2b4e2d84ce3b9a64
isiagi <isiagigeofrey0@gmail.com>
2023-09-29 19:57:28 +0300

    feat: Add editing

--- src/Components/pages/ItemCard.jsx
+++ src/Components/pages/ItemCard.jsx
@@ -1,27 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import testImg from "../../assets/images/trouser.jpg";
 import service from "../../baseURL";
-import EditModal from '../SupportUtils/EditModal';
+import EditModal from "../SupportUtils/EditModal";
 
-const ItemCard = (props) => {
+const ItemCard = ({ id, name, quantity, notes, ondelete }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
-
-  const itemEditHandler = async() => {
-    // Receive edit data here then send it via props to ItemList
+  const itemEditHandler = async (data) => {
     try {
-      const response = await service.patch(`/items/update-item/${props.itemId}`, {...editedItem})
-      // console.log('**** New Response ****')
-      // console.log(response.data)
-      setModalOpen(false)
-      setIsEditing(false)
-
-    } catch(error) {
-      console.log(error)
+      await service.patch(`/items/update-item/${id}`, {
+        ...data,
+      });
+      setModalOpen(false);
+      setIsEditing(false);
+    } catch (error) {
+      console.log(error);
     }
-  }
-
+  };
 
   const openModal = () => {
     setModalOpen(true);
@@ -31,38 +27,19 @@ const ItemCard = (props) => {
     setModalOpen(false);
   };
 
-  const saveChanges = (editedItem) => {
-    // Handle saving changes to the server
-    setModalOpen(false);
-    setIsEditing(false);
-  };
-
-  const [editedItem, setEditedItem] = useState({
-    name: props.name,
-    quantity: props.quantity,
-    notes: props.notes,
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditedItem((prev) => ({ ...prev, [name]: value }));
-  };
-
-  if(isEditing){
+  if (isEditing) {
     return (
       <EditModal
         isOpen={openModal}
         onClose={closeModal}
-        onSave={saveChanges}
-        currentItem={
-          {
-            name: props.name,
-            quantity: props.quantity,
-            notes: props.notes
-          }
-        }
-       />
-    )
+        onSave={itemEditHandler}
+        currentItem={{
+          name,
+          quantity,
+          notes,
+        }}
+      />
+    );
   }
 
   return (
@@ -77,55 +54,30 @@ const ItemCard = (props) => {
         </div>
         <div className="h-40 gap-1 max-w-xs">
           <div className="flex justify-between">
-            {isEditing ? (
-              <input
-                type="text"
-                name="name"
-                value={editedItem.name}
-                onChange={handleInputChange}
-                className="font-bold text-2xl"
-              />
-            ) : (
-              <h2 className="font-bold text-2xl">{props.name}</h2>
-            )}
+            <h2 className="font-bold text-2xl">{name}</h2>
+
             <p className="py-1 px-2 rounded-xl bg-gray-200 text-gray-500 text-sm">
-              {props.quantity} items
+              {quantity} items
             </p>
           </div>
-          <p className="w-80">
-            {isEditing ? (
-              <input
-                type="text"
-                name="notes"
-                value={editedItem.notes}
-                onChange={handleInputChange}
-              />
-            ) : (
-              props.notes
-            )}
-          </p>
+          <p className="w-80">{notes}</p>
           <div className="mt-8 gap-2 flex justify-between">
-            {isEditing ? (
-              <button 
-                onClick={itemEditHandler} 
-                className="flex px-4 py-3 bg-gray-100">
-                Save
-              </button>
-            ) : (
-              <button 
-                onClick={() => setIsEditing(true)} 
-                className="flex px-4 py-3 bg-gray-100">
-                Edit
-              </button>
-            )}
             <button
-              onClick= {props.ondelete}
-              className="flex px-4 py-3 bg-red-200 rounded-full">
+              onClick={() => setIsEditing(true)}
+              className="flex px-4 py-3 bg-gray-100"
+            >
+              Edit
+            </button>
+
+            <button
+              onClick={ondelete}
+              className="flex px-4 py-3 bg-red-200 rounded-full"
+            >
               Delete
             </button>
           </div>
         </div>
-      </div>      
+      </div>
     </div>
   );
 };
--- src/Components/pages/ItemsList.jsx
+++ src/Components/pages/ItemsList.jsx
@@ -3,79 +3,41 @@ import ItemCard from "./ItemCard.jsx";
 import service from "../../baseURL";
 
 const ItemsList = () => {
-    const [getItems,setGetItems]=useState([])
-    
-    useEffect(() => {
-        const getItems = async () => {
-            try {
-                const response = await service.get("items/get-items",{
-                    
-                });
-                console.log(response);
-                setGetItems(response)
-            } catch (error) {
-                
-                console.log(error.response.data.data);
-            }
-        };
-        
-        getItems();
-        
-    }, []);
-    // console.log({getItems})
+  const [getItems, setGetItems] = useState([]);
 
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await service.get("items/get-items", {});
+        setGetItems(response);
+      } catch (error) {
+        console.log(error.response.data.data);
+      }
+    };
 
+    fetchItems();
+  }, []);
 
-    // We pass the data via props
-    const editedDataHandler = () => {
-
+  // Setting up a delete Handler
+  const deleteItemHandler = async (id) => {
+    try {
+      await service.delete(`items/delete-item/${id}`);
+      alert("Item has been successfully deleted");
+    } catch (e) {
+      console.log(e.message);
     }
+  };
 
-
-
-    // Setting up a delete Handler
-    const deleteItemHandler = async(id) => {
-        // ev.preventDefault();
-
-        console.log(id)
-        // axios.delete('/delete-items/'+id).then(response => {
-        //     let {data} = response;
-
-        // })
-        try {
-            // console.log('Are you deleting')
-           const item2Del = await service.delete(`items/delete-items/${id}`)
-           console.log(item2Del)
-            // .then(
-            //     () => {
-            //         const newList = getItems.filter((el) => el.id !== id)
-            //         setGetItems(newList);
-            //     }
-            //)
-        } catch (e) {
-            console.log(e.message)
-        }
-    }
-
-
-    return (
-        <div className="md:ml-4 ">
-            {
-                getItems.data?.data?.length > 0 && getItems.data?.data?.map((item) => (
-                    <div className="border-b-[#F6F8FA] border-b-2 pb-4" key={item.id}>
-                    <ItemCard 
-                        name={item.name}
-                        quantity={item.quantity}
-                        notes={item.notes}
-                        onedit={editedDataHandler}
-                        ondelete={() => deleteItemHandler(item.id)}
-                    />
-                    </div>
-                ))
-            }
-        </div>
-    )
-}
+  return (
+    <div className="md:ml-4 ">
+      {getItems.data?.data?.length > 0 &&
+        getItems.data?.data?.map((item) => (
+          <div className="border-b-[#F6F8FA] border-b-2 pb-4" key={item.id}>
+            <ItemCard {...item} ondelete={() => deleteItemHandler(item.id)} />
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default ItemsList;
-
